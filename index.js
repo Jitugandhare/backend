c9b6e4dd44e750380022fa89e376eb48e91ed77b
Jitu gandhare <jitugandhare@gmail.com>
2024-12-28 14:09:05 +0530

    something fixing

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({extended:true}))
 // cors 
 
 app.use(cors({
-  origin: 'http://localhost:3000', 
+  origin: 'https://edgistify-lake.vercel.app/', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'], 
 }));
